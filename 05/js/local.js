ce448e12bf3521e4404bff3771a3b32d1df7aba4
Jung Young Hun <hujung0268@gamil.com>
2024-05-31 16:19:31 +0900

    modified:   05/js/local.js

--- 05/js/local.js
+++ 05/js/local.js
@@ -1,7 +1,8 @@
-var myVar = 100;
+// 함수 안에서만 쓸 수 있는 지역 변수
+var myVar = 100;	//전역 변수 선언
 test();
 document.write("myVar is " + myVar);
 
 function test() { 			
-	var myVar = 50;
+	var myVar = 50;	//지역 변수 선언
 }
