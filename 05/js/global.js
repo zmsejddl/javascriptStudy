0d65408dab8cd70f69567a41c57880dc82201a60
Jung Young Hun <hujung0268@gamil.com>
2024-05-31 17:56:10 +0900

    modified:   05/js/global.js 	modified:   05/js/hoisting.js

--- 05/js/global.js
+++ 05/js/global.js
@@ -1,7 +1,8 @@
-var myVar = 100;
+// 스크립트 안에서 자유롭게 쓸 수 있는 전역 변수
+var myVar = 100;	//전역 변수 선언
 test();
 document.write("myVar is " + myVar);
 
 function test() { 			
-	myVar = 50;
+	myVar = 50;	//전역 변수 선언
 }
