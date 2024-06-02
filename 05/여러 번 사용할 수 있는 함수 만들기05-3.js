bed7ffa2bbc7fc0be07fac585e3a935628bb5e5a
Jung Young Hun <hujung0268@gamil.com>
2024-06-02 16:52:20 +0900

    new file:   "05/\354\227\254\353\237\254 \353\262\210 \354\202\254\354\232\251\355\225\240 \354\210\230 \354\236\210\353\212\224 \355\225\250\354\210\230 \353\247\214\353\223\244\352\270\26005-3.js"

---
+++ 05/여러 번 사용할 수 있는 함수 만들기05-3.js
@@ -0,0 +1,26 @@
+// 함수 선언할 때 매개변수 지정하기
+// function addNumber(a, b) {
+//     var sum = a + b;
+//     console.log(sum);
+// }
+
+// addNumber(2, 3);
+// 5
+// addNumber(10, 20);
+// 30
+
+
+// 두 수를 더하는 함수 실행하기
+// var num1 = parseInt(prompt("첫 번째 숫자는? "));    //프롬프트 창에서 숫자 입력받기
+// var num2 = parseInt(prompt("두 번째 숫자는? "));    //프롬프트 창에서 숫자 입력받기
+// addNumber(num1, num2);  //num1, num2와 함께 addNumber() 함수 호출
+
+// function addNumber(a, b) {  //넘겨받은 값 num1은 a로, num2는 b로 전달
+//     var sum = a + b;    //a와 b를 연산해서 콘솔 창에 표시
+//     alert("두 수를 더한 값은 " + sum + "입니다");   //a와 b를 연산해서 콘솔 창에 표시
+// }
+
+// 1분 복습
+// 'addNumber(num1, num2) 함수 실행 소스는 함수 선언 소스보다 앞에 와야 합니다.'
+// 이 말이 맞다면 O, 틀리다면 X로 표시하세요
+// X (함수 선언 앞이나 뒤 어디에 놓여도 됩니다.)
