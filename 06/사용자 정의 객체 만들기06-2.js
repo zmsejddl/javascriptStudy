fe655a1680359b693f60640ff7371140ac7fa23e
Jung Young Hun <hujung0268@gamil.com>
2024-06-05 18:14:33 +0900

    new file:   "06/\354\202\254\354\232\251\354\236\220 \354\240\225\354\235\230 \352\260\235\354\262\264 \353\247\214\353\223\244\352\270\26006-2.js"

---
+++ 06/사용자 정의 객체 만들기06-2.js
@@ -0,0 +1,21 @@
+// 리터럴 표기법을 사용해 객체를 만드는 방법
+// var book = {    
+//     title: "자바스크립트",
+//     author: "고쌤",
+//     pages: 500,
+//     price: 15000,
+//     info: function() {
+//         alert(this.title + "책의 분량은 " + this.pages + "쪽입니다.");
+//     }
+// }
+// book - 객체
+// author - 속성 이름
+// "고쌤" - 속성 값
+// info - 함수 이름
+
+// 속성 값 가져오기
+// book.title
+// book.info()
+
+// 속성 추가
+// book.field = "IT"
