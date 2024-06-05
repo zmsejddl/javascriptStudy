762009c6616ad9b47439583a4b6f6acf1cc247d8
Jung Young Hun <hujung0268@gamil.com>
2024-06-05 18:28:00 +0900

    modified:   "06/\354\202\254\354\232\251\354\236\220 \354\240\225\354\235\230 \352\260\235\354\262\264 \353\247\214\353\223\244\352\270\26006-2.js"

--- 06/사용자 정의 객체 만들기06-2.js
+++ 06/사용자 정의 객체 만들기06-2.js
@@ -19,3 +19,15 @@
 
 // 속성 추가
 // book.field = "IT"
+
+
+// 생성자 함수를 사용해 객체를 만드는 방법
+// function Book(author, pages, price, title) {
+//     this.author = author;
+//     this.pages = pages;
+//     this.price = price;
+//     this.title = title;
+// }
+
+// jsBook = new Book("홍길동", 500, 15000, "자바스크립트") //인스턴스 만들기
+// jsBook.title    //jsBook 객체에서 속성 값 확인하기
