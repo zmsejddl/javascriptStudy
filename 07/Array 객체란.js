bb845485a28b9e4c0815e0c368654fefa368b5d8
Jung Young Hun <hujung0268@gamil.com>
2024-06-06 12:27:29 +0900

    new file:   "07/Array \352\260\235\354\262\264\353\236\200.js"

---
+++ 07/Array 객체란.js
@@ -0,0 +1,22 @@
+// Array 객체로 배열 만들기
+// var myArray = new Array();  //Array 객체의 인스턴스를 만듭니다
+// var numbers = ["one", "two", "three", "four"];  //리터럴을 사용한 배열
+// var numbers = new Array("one", "two", "three", "four")  //Array 객체를 사용한 배열
+
+
+// 배열에서 for문 사용하기
+// - length 속성
+// var seasons = ["봄", "여름", "가을", "겨울"]
+// seasons.length
+
+// - for문 실행
+// for(var i = 0; i < seasons.length; i++) {
+//     console.log(seasons[i]);
+// }
+
+// - 1분 복습
+// triple = [3, 6, 9] 배열이 있을 때 triple의 모든 값을 콘솔 창에 표시하는 소스를 작성하세요
+// 정답:
+// for(var i = 0; i < triple.length; i++) {
+//     console.log(triple[i]);
+// }
