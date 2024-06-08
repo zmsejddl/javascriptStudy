3a9125fbc1407fb54c3909c7d954665a10c48790
Jung Young Hun <hujung0268@gamil.com>
2024-06-08 13:05:48 +0900

    modified:   08/js/product.js 	modified:   08/product.html

--- 08/js/product.js
+++ 08/js/product.js
@@ -1,4 +1,4 @@
-		var bigPic = document.querySelector("#cup");  
+		var cup = document.querySelector("#cup");  
 		var smallPics = document.querySelectorAll(".small");  
 
 		for(i=0; i<smallPics.length; i++) {
@@ -7,3 +7,25 @@
 				bigPic.setAttribute("src", newPic);
 			});
 		}
+
+		// [상세 설명 보기] 버튼 만들기
+		var view = document.querySelector("#view");	//id="view"인 요소를 가져옴
+		// view.addEventListener("click", function() {	//view를 눌렀을 때 실행할 함수 선언
+		// 	document.querySelector("#detail").style.display = "block";	//detail 요소의 display 속성 값을 "block"으로 지정
+		// })
+
+		// [상세 설명 닫기] 버튼 만들기
+		var isOpen = false;
+		view.addEventListener("click", function() {
+			if(isOpen == false) {
+				//상세 정보가 감춰져 있을 때 실행할 소스 위치
+				document.querySelector("#detail").style.display = "block";	//상세 정보를 화면에 표시
+				view.innerHTML = "상세 설명 닫기";	//링크 내용 수정
+				isOpen = true;	//표시 상태로 지정
+			} else {
+				//상세 정보가 표시되어 있을 때 실행할 소스 위치
+				document.querySelector("#detail").style.display = "none";	//상세 정보를 화면에서 감춤
+				view.innerHTML = "상세 설명 보기";	//링크 내용 수정
+				isOpen = false;	//감춰진 상태로 지정
+			}
+		})
