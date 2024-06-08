307a463c9cc3c81c49e60a9e323d57612e43185a
Jung Young Hun <hujung0268@gamil.com>
2024-06-08 12:21:59 +0900

    new file:   "08/\354\233\271 \354\232\224\354\206\214\354\235\230 \355\203\234\352\267\270 \354\206\215\354\204\261 \352\260\200\354\240\270\354\231\200\354\204\234 \354\210\230\354\240\225\355\225\230\352\270\26008-3.js"

---
+++ 08/웹 요소의 태그 속성 가져와서 수정하기08-3.js
@@ -0,0 +1,5 @@
+// HTML 태그 속성을 가져오는 함수
+// document.querySelector("#prod-img > img").getAttribute("src")
+
+// HTML 태그 속성을 수정하는 함수
+// document.querySelector("#prod-img > img").setAttribute("src", "images/coffee-blue.jpg")
