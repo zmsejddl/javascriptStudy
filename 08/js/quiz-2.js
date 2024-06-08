3f2bf9d3c23245d1cf50d802323b25a557cbb57b
Jung Young Hun <hujung0268@gamil.com>
2024-06-08 14:00:17 +0900

    new file:   08/js/quiz-2.js 	modified:   08/quiz-2.html

---
+++ 08/js/quiz-2.js
@@ -0,0 +1,6 @@
+var checks = document.querySelectorAll(".check");
+for(var i = 0; i < checks.length; i++) {
+    checks[i].addEventListener("click", function() {
+        this.parentNode.style.color = "#eee"
+    })
+}
